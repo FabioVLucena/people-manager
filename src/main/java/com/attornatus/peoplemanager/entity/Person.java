53a15f05dd3ca0be18de3b7f7b5261a16efce2d0
aeon <fabio-trab@outlook.com>
2023-10-01 17:19:48 -0400

    changes the name of the entity people to person

--- src/main/java/com/attornatus/peoplemanager/entity/Person.java
+++ src/main/java/com/attornatus/peoplemanager/entity/Person.java
@@ -8,8 +8,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity(name = "people")
-public class People {
+@Entity(name = "person")
+public class Person {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
