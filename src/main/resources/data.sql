42e31188945746790521b99203ac8ea61f480f35
aeon <fabio-trab@outlook.com>
2023-10-01 23:56:08 -0400

    add data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,34 @@
+INSERT INTO person (name, birth_date) VALUES ('Marley', '2001-06-13');
+INSERT INTO person (name, birth_date) VALUES ('Carl', '2005-02-07');
+INSERT INTO person (name, birth_date) VALUES ('Astolf', '2010-10-19');
+INSERT INTO person (name, birth_date) VALUES ('Cristhian', '1997-03-01');
+
+INSERT INTO person_address (person_id, address, cep, number, main, city) 
+                    VALUES (1, 'Colorado Avenue', '899975', '68', 1, 'Austin');
+                    
+INSERT INTO person_address (person_id, address, cep, number, main, city) 
+                    VALUES (1, 'Richardson Avenue', '82385', '128', 0, 'Austin');
+                    
+INSERT INTO person_address (person_id, address, cep, number, main, city) 
+                    VALUES (1, 'Pacpi Avenue', '25675', '898', 0, 'Boston');                    
+
+                    
+INSERT INTO person_address (person_id, address, cep, number, main, city) 
+                    VALUES (2, 'Boemia Avenue', '899975', '68', 1, 'Austin');
+                    
+INSERT INTO person_address (person_id, address, cep, number, main, city) 
+                    VALUES (2, 'Proste Avenue', '82385', '128', 0, 'Austin');
+                    
+INSERT INTO person_address (person_id, address, cep, number, main, city) 
+                    VALUES (3, 'Help Avenue', '25675', '898', 0, 'Boston');                    
+                    
+
+INSERT INTO person_address (person_id, address, cep, number, main, city) 
+                    VALUES (3, 'Sad Avenue', '899975', '68', 1, 'Austin');
+                    
+INSERT INTO person_address (person_id, address, cep, number, main, city) 
+                    VALUES (3, 'Wumpalumpa Avenue', '82385', '128', 0, 'Austin');
+                    
+INSERT INTO person_address (person_id, address, cep, number, main, city) 
+                    VALUES (4, 'Gamescon Avenue', '25675', '898', 1, 'Boston');
+          
